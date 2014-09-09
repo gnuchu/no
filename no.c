890dfb3565233d86e8b7f64400fdcceb6e4b8d5d
John <gnuchu@gmail.com>
2014-09-09 19:00:44 +0100

    refactor

--- no.c
+++ no.c
@@ -2,22 +2,26 @@
 #include <string.h>
 #include <stdlib.h>
 
+#define INPUT_STRING 20
+
 int main(int argc, char *argv[])	{
 
-  char message[20];
-  memset(message, '\0', sizeof(message));
+  char * message = calloc(INPUT_STRING, sizeof(char));
 
   if(argc>1) {
-	strcpy(message, argv[1]);
+		strcpy(message, argv[1]);
   }
+	else {
+		strcpy(message, "n\0");
+	}
 
   while(1)	{
     if(argc>1) {
-	  printf("%s\n", message);
-	}
-	else {
-	  printf("n\n");
-	}
+	  	printf("%s\n", message);
+		}
+		else {
+	  	printf("n\n");
+		}
   } 
   return 0; 
 }
