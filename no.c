1b87d497ae0f4ad7cfccaddb7e02eb53448ca51c
John <gnuchu@gmail.com>
2014-09-09 14:37:44 +0100

    add message functionality like yes

--- no.c
+++ no.c
@@ -1,8 +1,23 @@
 #include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
 int main(int argc, char *argv[])	{
+
+  char message[20];
+  memset(message, '\0', sizeof(message));
+
+  if(argc>1) {
+	strcpy(message, argv[1]);
+  }
+
   while(1)	{
-    printf("n\n");
+    if(argc>1) {
+	  printf("%s\n", message);
+	}
+	else {
+	  printf("n\n");
+	}
   } 
   return 0; 
 }
